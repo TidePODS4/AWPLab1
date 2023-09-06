010c326222b44e8883713ce8263887f50fb316b3
Павел Баканов <bakanov2210@icloud.com>
2023-09-06 23:39:07 +0300

    added admin role and new constr to user

--- src/main/java/com/awp/lab1/entity/User.java
+++ src/main/java/com/awp/lab1/entity/User.java
@@ -1,11 +1,19 @@
 package com.awp.lab1.entity;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Setter;
 
 @Data
+@AllArgsConstructor
 public class User {
     private String login;
     private String password;
+    private boolean isAdmin;
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
 }
