54b2af0e8fb5fd813be2f4a955c7774abb544baf
Павел Баканов <bakanov2210@icloud.com>
2023-11-13 17:42:35 +0300

    minor changes

--- src/main/java/com/awp/lab1/servlet/LoginUserServlet.java
+++ src/main/java/com/awp/lab1/servlet/LoginUserServlet.java
@@ -10,23 +10,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Date;
 
 @WebServlet(name = "LoginUserServlet", value = "/login")
 public class LoginUserServlet extends HttpServlet {
-
-    /*@SneakyThrows
-    @Override
-    public void init(){
-        var admin = new User("admin", "admin", true);
-        UserTable.addUser(admin);
-    }
-*/
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        req.getRequestDispatcher("views/login.jsp").
-            forward(req, resp);
+        req.setCharacterEncoding("UTF-8");
+        req.getRequestDispatcher("views/login.jsp")
+                .forward(req, resp);
     }
 
     @Override
@@ -34,23 +26,24 @@ public class LoginUserServlet extends HttpServlet {
             throws ServletException, IOException {
         String login = req.getParameter("login");
         String password = req.getParameter("password");
+
         var user = new User(login, password);
 
         var optionalUser = UserTable.login(user);
-
         if (optionalUser.isPresent()){
             routing(req, resp, optionalUser.get());
             return;
         }
 
         req.setAttribute("isUserValid", false);
-        req.getRequestDispatcher("views/login-user.jsp").forward(req, resp);
+        req.getRequestDispatcher("views/login.jsp").forward(req, resp);
     }
 
     private static void routing(HttpServletRequest req, HttpServletResponse resp, User user)
-            throws IOException, ServletException {
+            throws IOException {
 
-        if (user.isAdmin()){
+        System.out.println(user);
+        if (user.getAdmin()){
             resp.sendRedirect(req.getContextPath() + "/admin-panel");
         }
         else {
