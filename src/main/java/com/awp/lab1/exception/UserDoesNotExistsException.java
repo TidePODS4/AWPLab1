48703cd3f16c52f3cc4b26e1f0318dcf57cd0484
Павел Баканов <bakanov2210@icloud.com>
2023-11-13 17:37:08 +0300

    renamed UserDoesNotExists.java

--- src/main/java/com/awp/lab1/exception/UserDoesNotExistsException.java
+++ src/main/java/com/awp/lab1/exception/UserDoesNotExistsException.java
@@ -2,10 +2,10 @@ package com.awp.lab1.exception;
 
 import com.awp.lab1.entity.User;
 
-public class UserDoesNotExists extends Exception{
+public class UserDoesNotExistsException extends Exception{
     private final User user;
 
-    public UserDoesNotExists(User user) {
+    public UserDoesNotExistsException(User user) {
         super("User " + user.getLogin() + "does not exists");
         this.user = user;
     }
