48f6a102d7d2b211d9e7da690334a1300f8db58c
Павел Баканов <bakanov2210@icloud.com>
2023-11-13 17:41:57 +0300

    implemented put, delete and post request handlers

--- src/main/java/com/awp/lab1/servlet/AdminPanelServlet.java
+++ src/main/java/com/awp/lab1/servlet/AdminPanelServlet.java
@@ -15,26 +15,57 @@ import java.io.IOException;
 public class AdminPanelServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("users", UserTable.getAllUsers());
         req.getRequestDispatcher("views/admin-panel.jsp")
             .forward(req, resp);
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String method = req.getParameter("_method");
+        if (method != null && method.equals("put")) {
+            doPut(req, resp);
+            return;
+        } else if (method != null && method.equals("delete")) {
+            doDelete(req, resp);
+            return;
+        }
         String login = req.getParameter("login");
         String password = req.getParameter("password");
-        boolean isAdmin = Boolean.parseBoolean(req.getParameter("isAdmin"));
+        var admin = req.getParameter("admin");
+        boolean isAdmin = "on".equals(admin);
 
         User user = new User(login, password, isAdmin);
 
         try {
             UserTable.addUser(user);
+            req.setAttribute("newUserLogin", user.getLogin());
         } catch (UserAlreadyExistsException e) {
             req.setAttribute("isExists", true);
-            req.getRequestDispatcher("views/admin-panel.jsp").forward(req, resp);
+        } finally {
+            resp.sendRedirect("/admin-panel");
         }
+    }
 
-        req.setAttribute("newUserLogin", user.getLogin());
-        req.getRequestDispatcher("views/admin-panel.jsp").forward(req, resp);
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+        var admin = req.getParameter("admin");
+        boolean isAdmin = "on".equals(admin);
+
+        User user = new User(login, password, isAdmin);
+
+        UserTable.update(user);
+
+        resp.sendRedirect("/admin-panel");
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String login = req.getParameter("login");
+        UserTable.deleteByLogin(login);
+
+        resp.sendRedirect("/admin-panel");
     }
 }
